000712c96b8a108e38fbed8d4b9fc3394ed5a42d
Joe Lapp <arachnojoe@gmail.com>
2022-01-08 08:09:25 -0600

    Added AwaitedType<T> to return the resolving type of an async function

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,7 @@ export { setIpcBindingTimeout } from "./shared_ipc";
 import { Recovery } from "./recovery";
 export type RecoverableClass<T> = Recovery.RecoverableClass<T>;
 export type RecoveryFunction = Recovery.RecoveryFunction;
+
+export type AwaitedType<T> = T extends (...args: any[]) => Promise<infer R>
+  ? R
+  : never;
