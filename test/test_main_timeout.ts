6acc1bec0b458084f4cfeb36c5c414f545ca3b6f
Joe Lapp <arachnojoe@gmail.com>
2022-01-09 22:53:05 -0600

    Simplified main timeout test

--- test/test_main_timeout.ts
+++ test/test_main_timeout.ts
@@ -10,64 +10,44 @@ const mochaPath = path.join(rootPath, "node_modules/.bin/electron-mocha");
 
 describe("window does not bind to API", () => {
   test("main times out waiting for first window binding", (done) => {
-    const timer = setTimeout(
-      () => assert.fail("Timed out waiting on child process"),
-      EXEC_TIMEOUT_MILLIS
-    );
-
-    // I was unable to override the uncaught exception in electron-mocha,
-    // neither via process.removeAllListeners() + process.on() nor via
-    // process.prependOnceListener(), so I test the output of electron-mocha.
-
-    const testScript = path.join(
+    const script = path.join(
       rootPath,
       "build/test/client/win1_main_timeout.js"
     );
-    const command = `node ${mochaPath} ${testScript} --timeout 8000`;
-    exec(command, (err, stdout, _stderr) => {
-      clearTimeout(timer);
-      if (err) {
-        if (
-          stdout.includes("Timed out waiting for main API 'MainApi2' to bind")
-        ) {
-          done();
-        } else {
-          done(Error(err + "..." + stdout));
-        }
-      } else {
-        assert.fail("Child process unexpectedly completed..." + stdout);
-      }
-    });
+    verifyTimeout(script, done);
   });
 
   test("main times out waiting for second window binding of same API", (done) => {
-    const timer = setTimeout(
-      () => assert.fail("Timed out waiting on child process"),
-      EXEC_TIMEOUT_MILLIS
-    );
-
-    // I was unable to override the uncaught exception in electron-mocha,
-    // neither via process.removeAllListeners() + process.on() nor via
-    // process.prependOnceListener(), so I test the output of electron-mocha.
-
-    const testScript = path.join(
+    const script = path.join(
       rootPath,
       "build/test/client/win2_main_timeout.js"
     );
-    const command = `node ${mochaPath} ${testScript} --timeout 8000`;
-    exec(command, (err, stdout, _stderr) => {
-      clearTimeout(timer);
-      if (err) {
-        if (
-          stdout.includes("Timed out waiting for main API 'MainApi2' to bind")
-        ) {
-          done();
-        } else {
-          done(Error(err + "..." + stdout));
-        }
-      } else {
-        assert.fail("Child process unexpectedly completed..." + stdout);
-      }
-    });
+    verifyTimeout(script, done);
   });
 });
+
+function verifyTimeout(testScript: string, done: Mocha.Done): void {
+  // I was unable to override the uncaught exception in electron-mocha,
+  // neither via process.removeAllListeners() + process.on() nor via
+  // process.prependOnceListener(), so I test the output of electron-mocha.
+
+  const timer = setTimeout(
+    () => assert.fail("Timed out waiting on child process"),
+    EXEC_TIMEOUT_MILLIS
+  );
+  const command = `node ${mochaPath} ${testScript} --timeout 8000`;
+  exec(command, (err, stdout, _stderr) => {
+    clearTimeout(timer);
+    if (err) {
+      if (
+        stdout.includes("Timed out waiting for main API 'MainApi2' to bind")
+      ) {
+        done();
+      } else {
+        done(Error(err + "..." + stdout));
+      }
+    } else {
+      assert.fail("Child process unexpectedly completed..." + stdout);
+    }
+  });
+}
